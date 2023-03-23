8b319775c9efd2bc453b92a9bb987c0be3330b85
SaliSharfman <86108478+SaliSharfman@users.noreply.github.com>
2023-03-23 12:05:54 -0400

    Add PID

---
+++ PID.py
@@ -0,0 +1,28 @@
+class PID:
+    def __init__(self, p, i, d, max_i):
+        self.P = p
+        self.I = i
+        self.D = d
+        self.integral = 0
+        self.max_i = max_i
+        self.first_run = True
+        self.last_error = 0
+
+    def update(self, error, dt):
+        if self.first_run:
+            self.last_error = error
+            self.first_run = False
+        self.integral += self.I * error *dt
+        diff = (error - self.last_error)/dt
+        const_integral = constrain(self.integral,self.max_i,-self.max_i)
+        control_out = self.P * error +self.D*diff+const_integral
+        self.last_error = error
+        return control_out
+
+
+def constrain(val, max , min):
+    if val > max:
+        return max
+    elif val < min:
+        return min
+    return val
